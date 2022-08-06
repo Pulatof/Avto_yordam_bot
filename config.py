1833cb030a83ebc78c8431bd35e71e2f1d3f978d
pulatov <pulatofffizzat@gamil.com>
2022-08-06 15:55:09 +0500

    My message

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-bot_token ='5151700549:AAFoNVuXTObQwdrBgU600GjFL2MPj8zUkns'  #asosiy
-# bot_token ='5433455312:AAHC_OaldqxUv1-pH5lzOU0ZY7p8b_Ip7S8'  #test
+# bot_token ='5151700549:AAFoNVuXTObQwdrBgU600GjFL2MPj8zUkns'  #asosiy
+bot_token ='5433455312:AAHC_OaldqxUv1-pH5lzOU0ZY7p8b_Ip7S8'  #test
